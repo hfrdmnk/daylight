ac02dabf1cef5c3458a665999ad3760bfbf6f86e
Dominik Hofer <hfrdmnk@gmail.com>
2023-10-13 17:56:50 +0200

    Basic app done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,22 +2,38 @@
 	import { onMount } from 'svelte';
 
 	export let data;
+	let locations = data.locations;
+	let currentDateTime = new Date();
 
-	let currentTimeFormatted = '';
-	let currentTime: Date;
+	type Location = (typeof locations)[0];
 
-	function updateTime() {
-		const time = new Date();
-		const hours = time.getHours();
-		const minutes = time.getMinutes();
-		const seconds = time.getSeconds();
-		currentTimeFormatted = `${hours}:${minutes}:${seconds}`;
-		currentTime = time;
+	function updateDateTime() {
+		currentDateTime = new Date();
+	}
+
+	function getTimeInfo(dateTime: Date, timezone: string, property: 'date' | 'time') {
+		const formatter = new Intl.DateTimeFormat('de-CH', {
+			timeZone: timezone,
+			year: 'numeric',
+			month: '2-digit',
+			day: '2-digit',
+			hour: 'numeric',
+			minute: 'numeric',
+			second: 'numeric'
+		});
+
+		const formattedDateTime = formatter.format(dateTime);
+		const [date, time] = formattedDateTime.split(', ');
+
+		if (property === 'date') {
+			return date;
+		} else {
+			return time;
+		}
 	}
 
 	onMount(() => {
-		// Call updateTime() every second
-		setInterval(updateTime, 1000);
+		setInterval(updateDateTime, 1000);
 	});
 </script>
 
@@ -25,15 +41,22 @@
 	<div class="py-8 flex-1 container flex flex-col items-center">
 		<h1 class="text-4xl font-bold text-center">Daylight</h1>
 		<main class="flex-1 flex items-center flex-wrap gap-8">
-			{#each data.locations as location}
-				<div class="p-8 flex flex-col gap-8 items-center">
-					<h2 class="text-3xl font-bold">{location.title}</h2>
-					{currentTimeFormatted}
+			{#each locations as location}
+				<div
+					class="p-16 pb-8 flex flex-col items-center border border-gray-200 rounded-md shadow-md bg-white/50 backdrop-blur-lg"
+				>
+					<h3 class="text-4xl tabular-nums mb-2">
+						{getTimeInfo(currentDateTime, location.timezone, 'time')}
+					</h3>
+					<h3 class="font-normal tabular-nums text-2xl">
+						{getTimeInfo(currentDateTime, location.timezone, 'date')}
+					</h3>
+					<h2 class="font-normal text-gray-500 text-xl mt-16">{location.title}</h2>
 				</div>
 			{/each}
 		</main>
 	</div>
 	<footer class="py-8">
-		<div class="container text-center">🐹😸🐠</div>
+		<div class="container text-center">🐹 😸 🐠</div>
 	</footer>
 </div>
