a4baaa14d73c78713e7126a71a0aff8b66571f69
Dominik Hofer <hfrdmnk@gmail.com>
2023-10-14 09:26:42 +0200

    Optimized marker size

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -56,7 +56,7 @@
 				const markers = location.markers.map((marker) => {
 					const tempMarker = {
 						location: [marker[0], marker[1]],
-						size: 0.1
+						size: 0.08
 					} as Marker;
 
 					return tempMarker;
